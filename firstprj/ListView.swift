d49451bf5b98c4954d323e7a2b83a214547ffb9a
bcyu <chanyb0827@gmail.com>
2024-06-13 15:23:54 +0900

    update: (ListView) multiselect 기능 +os_log 사용

--- firstprj/ListView.swift
+++ firstprj/ListView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import OSLog
 
 struct ListView : View {
+    
+    @State private var selectedFruits = Set<UUID>()
     
     struct Fruit : Identifiable {
         let id = UUID()
@@ -22,13 +25,28 @@ struct ListView : View {
     ]
     
     var body : some View {
-        List(fruits) { item in // .self 는 모든 인스턴스들이 암시적으로 생성하는 프로퍼티
+        List(fruits, selection: $selectedFruits) { item in // .self 는 모든 인스턴스들이 암시적으로 생성하는 프로퍼티
             HStack {
                 Circle()
                     .fill(item.color)
                     .frame(width: 20, height: 20)
                 Text(item.name)
             }
+        }.navigationBarTitle("Fruits")
+        .navigationBarItems(trailing: EditButton())
+        .environment(\.editMode, .constant(.active))
+        .onChange(of: selectedFruits) {
+            for uuid in selectedFruits {
+                _ = fruits.map { fruit in
+                    if fruit.id == uuid {
+                        let log = OSLog(subsystem: "kr.co.kworks.firstprj", category: "test")
+                        os_log(.default, log: log, "fruit: \(fruit.name), color: \(fruit.color)")
+                    }
+                    return fruit
+                }
+            }
         }
+        
+        
     }
 }
