1ec8e245074ac1185cbd0eb7e034d421fcc93ccb
bcyu <chanyb0827@gmail.com>
2024-06-13 11:22:28 +0900

    update: (NavigationView) closure 축약 변경

--- firstprj/NavigationViewView.swift
+++ firstprj/NavigationViewView.swift
@@ -8,44 +8,58 @@
 import SwiftUI
 struct NavigationViewView: View {
     
-    @State var fruits = [Fruit(name: "Apple"), Fruit(name: "Banana")]
+    @State var fruits = [Fruit(name: "Apple"), Fruit(name: "Banana"), Fruit(name: "Grape")]
+    @State var text: String = ""
+    @State private var selection = Fruit(name: "fruit")
     
     var body: some View {
         NavigationView {
             VStack {
-                List {
-                    ForEach(fruits) { fruit in
-                        NavigationLink(destination: DetailView()) {
-                            Text(fruit.name)
-                        }
-                    }.onDelete(perform: del)
+                List(fruits, rowContent: fruitView)
 
-                }
                 Button(action: add) {
                     Text("Add")
-                }
+                }.toolbar{ EditButton() }
+                
+            
+                Text("Selected: \(selection)")
             }
-        
         }
     }
     
+    // 클로저를 변수로 정의
+    let fruitView: (Fruit) -> AnyView = { fruit in
+        AnyView( // AnyView
+            HStack {
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 20, height: 20)
+                Text(fruit.name)
+            }
+        )
+    }
+    
+    
     func add() {
+        text = "asdfff"
         fruits.append(Fruit(name: "temp"))
     }
     
     func del(offset: IndexSet) {
         fruits.remove(atOffsets: offset)
     }
+
 }
 
-struct Fruit: Identifiable {
+struct Fruit: Identifiable, Hashable {
     let id = UUID()
     let name: String
 }
 
 struct DetailView: View {
+    @Binding var text: String
     var body: some View {
-        Text("This is the Detail View")
+        Text("This is the Detail View \(text)")
             .navigationTitle("Detail")
     }
 }
