93e0eece27d7cc23276901ae1c382452b6e3ffc3
bcyu <chanyb0827@gmail.com>
2024-06-18 15:39:28 +0900

    add: (UsePackage) pakage dependencies 사용

---
+++ firstprj/UsePackage.swift
@@ -0,0 +1,18 @@
+//
+//  UsePackage.swift
+//  firstprj
+//
+//  Created by 유병찬 on 6/18/24.
+//
+
+import SwiftUI
+import FabulaItemsProvider
+
+struct UsePackage: View {
+    var body: some View {
+        VStack{
+            P23_Waves()
+        }
+        
+    }
+}
